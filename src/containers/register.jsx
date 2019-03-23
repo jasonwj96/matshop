6b7ff02f58bd6b35695fdf0e75cee2fe287bd1d7
Jason Wedderburn <JasonAlejandro507@hotmail.com>
2019-03-23 17:13:56 -0500

    Added input handlers to the registration form

--- src/containers/register.jsx
+++ src/containers/register.jsx
@@ -11,11 +11,71 @@ export default class Register extends Component {
         lastName: false,
         email: false,
         password: false
-      }
+      },
+      firstName: "",
+      lastName: "",
+      email: "",
+      address: "",
+      password: "",
+      confirmPassword: ""
     };
   }
 
   registerUser = () => alert("Registered user");
+
+  handleFirstNameChange = event => {
+    this.setState(
+      {
+        firstName: event.target.value
+      },
+      () => {}
+    );
+  };
+
+  handleLastNameChange = event => {
+    this.setState(
+      {
+        lastName: event.target.value
+      },
+      () => {}
+    );
+  };
+
+  handleEmailChange = event => {
+    this.setState(
+      {
+        email: event.target.value
+      },
+      () => {}
+    );
+  };
+
+  handleAddressChange = event => {
+    this.setState(
+      {
+        address: event.target.value
+      },
+      () => {}
+    );
+  };
+
+  handlePasswordChange = event => {
+    this.setState(
+      {
+        password: event.target.value
+      },
+      () => {}
+    );
+  };
+
+  handlePasswordConfirmChange = event => {
+    this.setState(
+      {
+        confirmPassword: event.target.value
+      },
+      () => {}
+    );
+  };
 
   render() {
     return (
@@ -28,11 +88,23 @@ export default class Register extends Component {
                 First name
                 <div className="requiredDiv" />
               </div>
-              <input type="text" name="firstName" placeholder="Bruce" />
+              <input
+                type="text"
+                value={this.state.firstName}
+                name="firstName"
+                placeholder="Bruce"
+                onChange={this.handleFirstNameChange}
+              />
             </label>
             <label htmlFor="lastName">
               <div>Last name</div>
-              <input type="text" name="lastName" placeholder="Wayne" />
+              <input
+                type="text"
+                value={this.state.lastName}
+                name="lastName"
+                placeholder="Wayne"
+                onChange={this.handleLastNameChange}
+              />
             </label>
             <label htmlFor="email">
               <div>
@@ -42,7 +114,9 @@ export default class Register extends Component {
               <input
                 type="email"
                 name="email"
-                placeholder="brucewayne@gmail.com"
+                value={this.state.email}
+                placeholder="notbatman@gmail.com"
+                onChange={this.handleEmailChange}
               />
             </label>
             <label htmlFor="address">
@@ -50,7 +124,9 @@ export default class Register extends Component {
               <textarea
                 type="text"
                 name="address"
-                placeholder="Wayne Manor, 1007 Mountain Drive, Gotham City. "
+                value={this.state.address}
+                placeholder="Wayne Manor, 1007 Mountain Drive, Gotham City"
+                onChange={this.handleAddressChange}
               />
             </label>
             <label htmlFor="password">
@@ -58,14 +134,24 @@ export default class Register extends Component {
                 Password
                 <div className="requiredDiv" />
               </div>
-              <input type="password" name="password" />
+              <input
+                type="password"
+                value={this.state.password}
+                name="password"
+                onChange={this.handlePasswordChange}
+              />
             </label>
             <label htmlFor="confirmPassword">
               <div>
                 Confirm password
                 <div className="requiredDiv" />
               </div>
-              <input type="password" name="confirmPassword" />
+              <input
+                type="password"
+                value={this.state.confirmPassword}
+                name="confirmPassword"
+                onChange={this.handlePasswordConfirmChange}
+              />
             </label>
             <div className="form-footer">
               <div>
