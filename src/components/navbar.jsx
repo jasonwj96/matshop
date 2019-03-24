4fa4f6d5a80abadb23773e6b3164c136ffa79800
Jason Wedderburn <JasonAlejandro507@hotmail.com>
2019-03-24 13:26:08 -0500

    Added react hooks to the Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,76 +1,69 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 
-export default class Navbar extends Component {
-  constructor(props) {
-    super(props);
+const Navbar = props => {
+  const [showMenu, setShowMenu] = useState(false);
 
-    this.state = {
-      showMenu: true
+  useEffect(() => {
+    return () => {
+      changeClass();
     };
-  }
+  }, [showMenu]);
 
-  showMenu = () => {
-    this.setState({ showMenu: !this.state.showMenu }, () => {
-      const container = document.getElementById("navbar-container");
-
-      if (this.state.showMenu) {
-        container.className = "expand-nav";
-      } else {
-        container.className = "collapse-nav";
-      }
-    });
+  const changeClass = () => {
+    const container = document.getElementById("navbar-container");
+    if (showMenu) {
+      container.className = "expand-nav";
+    } else {
+      container.className = "collapse-nav";
+    }
   };
 
-  render() {
-    return (
-      <div className="navbar">
-        <div id="navbar-container">
-          <div className="nav-menu">
-            <button id="nav-btn" onClick={this.showMenu}>
-              <div>
-                <div className="bar" />
-                <div className="bar" />
-                <div className="bar" />
-                <span />
-              </div>
-            </button>
+  const toggle = () => {
+    setShowMenu(!showMenu);
+  };
 
-            <div className="logo">Matshop</div>
-          </div>
-          <div />
-          <ul>
-            <Link onClick={this.showMenu} className="link" to="/Home">
-              Home
-            </Link>
+  return (
+    <div className="navbar">
+      <div id="navbar-container">
+        <div className="nav-menu">
+          <button id="nav-btn" onClick={toggle}>
+            <div>
+              <div className="bar" />
+              <div className="bar" />
+              <div className="bar" />
+              <span />
+            </div>
+          </button>
 
-            <Link onClick={this.showMenu} className="link" to="category?c=Pets">
-              Pets
-            </Link>
-
-            <Link
-              onClick={this.showMenu}
-              className="link"
-              to="category?c=Clothes"
-            >
-              Clothes
-            </Link>
-
-            <Link
-              onClick={this.showMenu}
-              className="link"
-              to="category?c=Books"
-            >
-              Books
-            </Link>
-
-            <Link onClick={this.showMenu} className="link" to="category?c=Tech">
-              Tech
-            </Link>
-          </ul>
+          <div className="logo">Matshop</div>
         </div>
+        <div />
+        <ul>
+          <Link onClick={toggle} className="link" to="/Home">
+            Home
+          </Link>
+
+          <Link onClick={toggle} className="link" to="category?c=Pets">
+            Pets
+          </Link>
+
+          <Link onClick={toggle} className="link" to="category?c=Clothes">
+            Clothes
+          </Link>
+
+          <Link onClick={toggle} className="link" to="category?c=Books">
+            Books
+          </Link>
+
+          <Link onClick={toggle} className="link" to="category?c=Tech">
+            Tech
+          </Link>
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Navbar;
