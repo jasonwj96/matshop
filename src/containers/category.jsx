fe6534f044504050b84319a297d91f1cefc7f7e3
Jason Wedderburn <JasonAlejandro507@hotmail.com>
2019-03-25 18:03:25 -0500

    Now category headers change properly

--- src/containers/category.jsx
+++ src/containers/category.jsx
@@ -1,13 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./category.scss";
 import Navbar from "../components/navbar";
 import Statusbar from "../components/statusbar";
 import Footer from "../components/footer";
 
 const Category = props => {
-  const [header] = useState("Hello world");
+  const [category, setCategory] = useState(props.match.params.c);
+  const [header, setHeader] = useState("");
 
-  const selectHeader = () => {};
+  useEffect(() => {
+    setCategory(props.match.params.c);
+    selectHeader();
+  });
+
+  const selectHeader = () => {
+    switch (category) {
+      case "pets":
+        setHeader("Items for man's best friends");
+        break;
+      case "clothes":
+        setHeader("The latest fashion trends");
+        break;
+      case "books":
+        setHeader("Your favorite book authors in one place");
+        break;
+      case "tech":
+        setHeader("Cutting edge tech at the tip of your hands");
+        break;
+      default:
+        props.history.push("/");
+        break;
+    }
+
+    return "";
+  };
 
   const content = (
     <div className="category-container">
