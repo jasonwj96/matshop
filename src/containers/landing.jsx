92d47ebd40a6014ac5f50c8a66d889e525c8e36d
Jason Wedderburn <JasonAlejandro507@hotmail.com>
2019-03-27 10:31:19 -0500

    Landing page is now a functional component

--- src/containers/landing.jsx
+++ src/containers/landing.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./landing.scss";
 import LandingSection from "../components/landingSection";
 import configuration from "../config";
@@ -7,88 +7,86 @@ import Statusbar from "../components/statusbar";
 import Footer from "../components/footer";
 import OfferPanel from "../components/offerPanel";
 
-export default class Landing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-      userLoggedIn: localStorage.getItem("userEmail") ? true : false
+const Landing = () => {
+  const [products, setProducts] = useState([]);
+  const controller = new AbortController();
+  document.title = "Matshop - Home";
+
+  useEffect(() => {
+    fetchProducts();
+    return () => {
+      controller.abort();
     };
-  }
+  }, []);
 
-  async componentDidMount() {
-    document.title = "Matshop - Home";
-
+  const fetchProducts = async () => {
     try {
       const response = await fetch(`${configuration.apiPath}/Home`);
 
       if (!response.ok) throw new Error("The products are gone ¯\\_(ツ)_/¯.");
 
       const products = await response.json();
-      this.setState({ products });
+
+      setProducts(products);
     } catch (err) {
       console.log(err.message);
     }
-  }
+  };
 
-  render() {
-    const offerItem = {
-      name: "Apple iPhone X, GSM Unlocked, 64GB - Silver",
-      description:
-        "All-screen design. Longest battery life ever in an iPhone. Fastest performance. Water and splash resistant. Studio-quality photos and 4K video. More secure with Face ID. The new iPhone XR. It’s a brilliant upgrade.",
-      imageUrl: "./product4.png",
-      alt: "iPhone",
-      price: 899.99
-    };
+  const offerItem = {
+    name: "Apple iPhone X, GSM Unlocked, 64GB - Silver",
+    description:
+      "All-screen design. Longest battery life ever in an iPhone. Fastest performance. Water and splash resistant. Studio-quality photos and 4K video. More secure with Face ID. The new iPhone XR. It’s a brilliant upgrade.",
+    imageUrl: "./product4.png",
+    alt: "iPhone",
+    price: 899.99
+  };
 
-    return (
-      <div>
-        <Navbar />
-        <Statusbar />
-        <div className="landing">
-          <div className="cover">
-            <div className="overlay">
-              <div className="cover-section">Welcome to Matshop!</div>
-              <div className="cover-section">It's nice to meet you</div>
-            </div>
+  const content = (
+    <div>
+      <Navbar />
+      <Statusbar />
+      <div className="landing">
+        <div className="cover">
+          <div className="overlay">
+            <div className="cover-section">Welcome to Matshop!</div>
+            <div className="cover-section">It's nice to meet you</div>
           </div>
-          {this.state.products.length === 0 ? (
-            <div className="spinner">
-              <i className="fas fa-spinner" />
-            </div>
-          ) : (
-            <LandingSection
-              heading={"What's hot right now"}
-              products={this.state.products}
-            />
-          )}
-
-          <OfferPanel item={offerItem} />
-
-          {this.state.products.length === 0 ? (
-            <div className="spinner">
-              <i className="fas fa-spinner" />
-            </div>
-          ) : (
-            <LandingSection
-              heading={"Men's clothing"}
-              products={this.state.products}
-            />
-          )}
-
-          {this.state.products.length === 0 ? (
-            <div className="spinner">
-              <i className="fas fa-spinner" />
-            </div>
-          ) : (
-            <LandingSection
-              heading={"Latest in tech"}
-              products={this.state.products}
-            />
-          )}
         </div>
-        <Footer />
+        {products.length === 0 ? (
+          <div className="spinner">
+            <i className="fas fa-spinner" />
+          </div>
+        ) : (
+          <LandingSection
+            heading={"What's hot right now"}
+            products={products}
+          />
+        )}
+
+        <OfferPanel item={offerItem} />
+
+        {products.length === 0 ? (
+          <div className="spinner">
+            <i className="fas fa-spinner" />
+          </div>
+        ) : (
+          <LandingSection heading={"Men's clothing"} products={products} />
+        )}
+
+        {products.length === 0 ? (
+          <div className="spinner">
+            <i className="fas fa-spinner" />
+          </div>
+        ) : (
+          <LandingSection heading={"Latest in tech"} products={products} />
+        )}
       </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+  );
+
+  return content;
+};
+
+export default Landing;
