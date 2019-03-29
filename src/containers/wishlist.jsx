746576efec99afb7d8ee3875969e17eb1ae85a57
Jason Wedderburn <JasonAlejandro507@hotmail.com>
2019-03-29 08:53:44 -0500

    Implemented hooks in the wishlist component

--- src/containers/wishlist.jsx
+++ src/containers/wishlist.jsx
@@ -1,127 +1,70 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./wishlist.scss";
 import Navbar from "../components/navbar";
 import WishlistItem from "../components/wishlistItem";
 import Statusbar from "../components/statusbar";
 
-export default class Wishlist extends Component {
-  constructor(props) {
-    super(props);
+const Component = props => {
+  [items, setItems] = useState([
+    {
+      imageUrl: "./product1.jpg",
+      title: "Lightweight Varsity Jacket",
+      quantity: 5,
+      alt: "Jacket",
+      rating: 89,
+      price: 19.99
+    },
+    {
+      imageUrl: "./product2.jpg",
+      title: "Apple iPhone X, GSM Unlocked, 64GB - Silver (Refurbished)",
+      description:
+        "All-screen design. Longest battery life ever in an iPhone. Fastest performance. Water and splash resistant. Studio-quality photos and 4K video. More secure with Face ID. The new iPhone XR. It’s a brilliant upgrade.",
+      alt: "IPhone",
+      quantity: 5,
+      rating: 39,
+      price: 899.99
+    },
+    {
+      imageUrl: "./product3.jpg",
+      title: "PlayStation 4 Slim 1TB Console",
+      quantity: 9,
+      alt: "Playstation 4",
+      rating: 73,
+      price: 299.99
+    }
+  ]);
 
-    this.state = {
-      items: [
-        {
-          imageUrl: "./product1.jpg",
-          title: "Lightweight Varsity Jacket",
-          quantity: 5,
-          alt: "Jacket",
-          rating: 89,
-          price: 19.99
-        },
-        {
-          imageUrl: "./product2.jpg",
-          title: "Apple iPhone X, GSM Unlocked, 64GB - Silver (Refurbished)",
-          description:
-            "All-screen design. Longest battery life ever in an iPhone. Fastest performance. Water and splash resistant. Studio-quality photos and 4K video. More secure with Face ID. The new iPhone XR. It’s a brilliant upgrade.",
-          alt: "IPhone",
-          quantity: 5,
-          rating: 39,
-          price: 899.99
-        },
-        {
-          imageUrl: "./product3.jpg",
-          title: "PlayStation 4 Slim 1TB Console",
-          quantity: 9,
-          alt: "Playstation 4",
-          rating: 73,
-          price: 299.99
-        },
-        {
-          imageUrl: "./product1.jpg",
-          title: "Lightweight Varsity Jacket",
-          quantity: 5,
-          alt: "Jacket",
-          rating: 89,
-          price: 19.99
-        },
-        {
-          imageUrl: "./product2.jpg",
-          title: "Apple iPhone X, GSM Unlocked, 64GB - Silver (Refurbished)",
-          description:
-            "All-screen design. Longest battery life ever in an iPhone. Fastest performance. Water and splash resistant. Studio-quality photos and 4K video. More secure with Face ID. The new iPhone XR. It’s a brilliant upgrade.",
-          alt: "IPhone",
-          quantity: 5,
-          rating: 39,
-          price: 899.99
-        },
-        {
-          imageUrl: "./product3.jpg",
-          title: "PlayStation 4 Slim 1TB Console",
-          quantity: 9,
-          alt: "Playstation 4",
-          rating: 73,
-          price: 299.99
-        },
-        {
-          imageUrl: "./product1.jpg",
-          title: "Lightweight Varsity Jacket",
-          quantity: 5,
-          alt: "Jacket",
-          rating: 89,
-          price: 19.99
-        },
-        {
-          imageUrl: "./product2.jpg",
-          title: "Apple iPhone X, GSM Unlocked, 64GB - Silver (Refurbished)",
-          description:
-            "All-screen design. Longest battery life ever in an iPhone. Fastest performance. Water and splash resistant. Studio-quality photos and 4K video. More secure with Face ID. The new iPhone XR. It’s a brilliant upgrade.",
-          quantity: 5,
-          alt: "IPhone",
-          rating: 39,
-          price: 899.99
-        },
-        {
-          imageUrl: "./product3.jpg",
-          title: "PlayStation 4 Slim 1TB Console",
-          quantity: 9,
-          alt: "Playstation 4",
-          rating: 73,
-          price: 299.99
-        }
-      ]
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     document.title = "Matshop - Wishlist";
-  }
+  }, []);
 
   checkoutItems = () => console.log("Items checked out");
 
-  render() {
-    return (
-      <div className="wishlist-container">
-        <Navbar />
-        <Statusbar />
-        <div className="wishlist">
-          <div className="header">
-            <p>My wishlist items</p>
-          </div>
-          <div>
-            {this.state.items.length > 0 ? (
-              this.state.items.map((item, index) => {
-                return <WishlistItem key={index} item={item} />;
-              })
-            ) : (
-              <p>No items in your wishlist</p>
-            )}
-          </div>
-          <div className="footer">
-            <button onClick={this.checkoutItems}>Clear</button>
-            <button onClick={this.checkoutItems}>Checkout</button>
-          </div>
+  const content = (
+    <div className="wishlist-container">
+      <Navbar />
+      <Statusbar />
+      <div className="wishlist">
+        <div className="header">
+          <p>My wishlist items</p>
+        </div>
+        <div>
+          {items.length > 0 ? (
+            items.map((item, index) => {
+              return <WishlistItem key={index} item={item} />;
+            })
+          ) : (
+            <p>No items in your wishlist</p>
+          )}
+        </div>
+        <div className="footer">
+          <button onClick={checkoutItems}>Clear</button>
+          <button onClick={checkoutItems}>Checkout</button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+  return content;
+};
+
+export default Wishlist;
