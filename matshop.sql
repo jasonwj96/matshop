d18c69abd81d2a6e26f7f1ed3ec1e4e4d10e7c6f
Jason Wedderburn <jasonwj96@gmail.com>
2019-12-12 15:58:56 -0500

    Fixed css consistency

--- matshop.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `matshop`.`products` ( `id` INT NOT NULL AUTO_INCREMENT , `product_id` VARCHAR(255) NOT NULL , `product_url` VARCHAR(255) NOT NULL , `title` VARCHAR(255) NOT NULL , `description` VARCHAR(255) NOT NULL , `alt` VARCHAR(255) NOT NULL , `rating` INT(255) NOT NULL , `price` DECIMAL(2) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-
-`root`@`localhost`
